a23e30f8797f8b762a47b9768da8aaf1e2e525ef
Thymen <thymenyj@gmail.com>
2018-06-07 08:30:16 +0200

    login concept updated

--- app/src/main/java/com/example/thymen/positivenews/LoginActivity.java
+++ app/src/main/java/com/example/thymen/positivenews/LoginActivity.java
@@ -1,13 +1,16 @@
 package com.example.thymen.positivenews;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,11 +21,21 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
-    public ImageView loginLogin, loginRegister;
-    public EditText loginUsername, loginPassword;
-    public TextView loginReset;
+    public Button loginLogin, loginRegister, loginReset;
+    public TextView loginUsername, loginPassword;
     public FirebaseAuth firebaseAuth;
     public FirebaseAuth.AuthStateListener authStateListener;
+
+    public RelativeLayout rellay1, rellay2;
+
+    Handler handler = new Handler();
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            rellay1.setVisibility(View.VISIBLE);
+            rellay2.setVisibility(View.VISIBLE);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +57,13 @@ public class LoginActivity extends AppCompatActivity {
             }
         };
 
-        loginUsername = findViewById(R.id.loginName);
+
+        rellay1 = findViewById(R.id.rellay1);
+        rellay2 = findViewById(R.id.rellay2);
+
+        handler.postDelayed(runnable, 2000); //2000 is the timeout for the splash
+
+        loginUsername = findViewById(R.id.loginUsername);
         loginPassword = findViewById(R.id.loginPassword);
         loginLogin = findViewById(R.id.loginLogin);
         loginRegister = findViewById(R.id.loginRegister);
